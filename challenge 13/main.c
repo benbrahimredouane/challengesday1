29b87b550b0cf2a8e823791661a04af6427b7848
Redouane <benbrahimredouane664.com>
2025-09-03 14:32:53 +0100

    pushing challenge 13 not finished yet

---
+++ challenge 13/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int decimal;
+    int  bites[32];
+    int i = 0 ;
+
+    printf("donne votre number en dicimal!\n");
+    scanf("%d",&decimal);
+
+    if (number == 0) {printf("0");
+    else {
+        while (number > 0){
+            bits[i++] = number % 2 ;
+            number = number / 2;
+        }
+        for (int)
+    }
+
+
+
+
+
+     printf("format hexadécimal est:%x",decimal);
+
+
+
+    return 0;
+}
