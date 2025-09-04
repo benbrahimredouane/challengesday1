bd8b85ee80a952bcea3cf27a1df6552c300522de
Redouane <benbrahimredouane664.com>
2025-09-04 10:19:46 +0100

    finish challenge 13

--- challenge 13/main.c
+++ challenge 13/main.c
@@ -3,27 +3,32 @@
 
 int main()
 {
-    int decimal;
-    int  bites[32];
-    int i = 0 ;
+    int decimal , temp , binaire[32];
+    int i = 0;
 
     printf("donne votre number en dicimal!\n");
     scanf("%d",&decimal);
+     printf("format hexadecimal est :%x \n",decimal);
 
-    if (number == 0) {printf("0");
-    else {
-        while (number > 0){
-            bits[i++] = number % 2 ;
-            number = number / 2;
-        }
-        for (int)
+    printf("votre number en binair est: \n") ;
+
+    temp = decimal;
+    if ( temp == 0 ) printf("votre number en binair est:");
+    else{
+    while(temp!=0) {
+
+
+            binaire[i] = temp % 2;
+            temp = temp / 2;
+            i++;
+
     }
 
-
-
-
-
-     printf("format hexadécimal est:%x",decimal);
+    }
+      for (int j = i - 1; j >= 0; j--) {
+            printf("%d", binaire[j]);
+        }
+        printf("\n");
 
 
 
